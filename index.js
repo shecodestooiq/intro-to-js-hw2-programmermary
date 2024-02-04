a58d58fa465736c424a19e475ecb109e4f18e493
unknown <maryamihsan601@gmail.com>
2024-02-04 18:42:27 +0300

    this is second js homework

---
+++ index.js
@@ -0,0 +1,9 @@
+
+let number1 = 30 ;
+let number2 = 25;
+console.log('less than' , number1<number2  )
+console.log('greater than' , number1>number2 )
+console.log('equal' , number1 == number2  )
+console.log('greater or equal' , number1 >= number2  )
+console.log('less or equal' , number1 <= number2  )
+console.log('NOT equal' , number1 != number2  )
